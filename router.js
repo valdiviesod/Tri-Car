26d372be45f3458a230068bb466ff592e287c29c
Daniel Valdivieso <dzvaldiviesod@gmail.com>
2022-05-03 11:45:53 -0500

    CRUD configurado

---
+++ router.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const connection = require('./database/db');
+const router = express.Router();
+
+router.get('/admin', (req,res) => {
+    connection.query('SELECT * FROM users', (error,results) => {
+        if(error){
+            throw error;
+        }else{
+            res.render('admin', {results:results})
+        }
+    })
+})
+
+router.get('/create', (req,res) => {
+    res.render('create');
+})
+
+router.get('/edit/:id', (req,res)=>{    
+    const id = req.params.id;
+    connection.query('SELECT * FROM users WHERE id=?',[id] , (error, results) => {
+        if (error) {
+            throw error;
+        }else{            
+            res.render('edit', {user:results[0]});            
+        }        
+    });
+});
+
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    connectionnodemo.query('DELETE FROM users WHERE id = ?',[id], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('admin');         
+        }
+    })
+});
+
+module.exports = router;
+
+const crud = require('./controllers/crud');
+router.post('/save', crud.save)
+router.post('/update',crud.update)
